91082090eff214a0600aeccd78554dd25dbc869e
soeun <separk314@naver.com>
2023-08-29 23:42:39 +0900

    Chore: 프로젝트 초기화

---
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(id);     // 영속상태의 엔티티를 찾아옴
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+
+//        itemRepository.save(findItem);
+        /*
+            영속 상태의 엔티티를 수정했으므로 다시 save를 호출할 필요 없다.
+            JPA가 자동으로 변경 감지 후 플러시, update query를 날림.
+         */
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
